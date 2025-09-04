e1ce00daf01c4876a93ea4d514f27620a406609e
Daniel Severo <daniel.severo@picpay.com>
2025-09-03 21:18:56 -0300

    (buil) Introducing routes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home/home";
 import Routes from "./routes/routes";
+import "./App.css"
 function App() {
   return <Routes/>
 }
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,9 +1,11 @@
 import "../Navbar/navbar.css"
-
+import logo_eitruck from "../../assets/logo_eitruck.svg"
+import icone_perfil from "../../assets/icone_perfil.png"
 export default function Navbar(){
     return(
         <nav>
-            
+            <img src={logo_eitruck} id="logo"/>
+            <img src={icone_perfil} id="icone_perfil"/>
         </nav>
     )
 }
